e495947991505cd224e8f1b69c0e2a22971dba47
taras.parfeniuk <taras.parfeniuk@gmail.com>
2021-02-27 15:02:57 +0200

    add configs

---
+++ src/config/sorting.ts
@@ -0,0 +1,15 @@
+export const demo = {
+    minSpeed: 1,
+    maxSpeed: 100,
+    stepInterval: 500
+};
+
+export const array = {
+    minElement: -10,
+    maxElement: 10,
+    minSize: 2,
+    maxSize: 250
+};
+
+Object.freeze(array);
+Object.freeze(demo);
